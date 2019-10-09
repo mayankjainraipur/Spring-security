0abc5eb7f674a9cf4c525bc1f7e1ee43cf581fbb
athorat <athorat@mocana.com>
2019-10-09 19:09:59 +0530

    java code change

--- src/main/java/com/login/security/Config/SecurityConfig.java
+++ src/main/java/com/login/security/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.dboperation.interection.Config;
+package com.login.security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,33 +9,31 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-public class SecurityConfig {
-    @EnableWebSecurity
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
-        // @formatter:off
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests(authorizeRequests ->
-                            authorizeRequests
-                                    .antMatchers("/css/**", "/index").permitAll()
-                                    .antMatchers("/user/**").hasRole("USER")
-                    )
-                    .formLogin(formLogin ->
-                            formLogin
-                                    .loginPage("/login")
-                                    .failureUrl("/login-error")
-                    );
-        }
-        // @formatter:on
-        @Bean
-        public UserDetailsService userDetailsService() {
-            UserDetails userDetails = User.withDefaultPasswordEncoder()
-                    .username("user")
-                    .password("password")
-                    .roles("USER")
-                    .build();
-            return new InMemoryUserDetailsManager(userDetails);
-        }
+@EnableWebSecurity
+public class SecurityConfig extends  WebSecurityConfigurerAdapter{
+    // @formatter:off
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/css/**", "/index").permitAll()
+                                .antMatchers("/user/**").hasRole("USER")
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .failureUrl("/login-error")
+                );
+    }
+    // @formatter:on
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
