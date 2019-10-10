bb192f36ec8f5d3bcfb29b18e194f1ce15c21fd3
athorat <athorat@mocana.com>
2019-10-10 13:52:01 +0530

    constants

---
+++ src/main/java/com/login/security/controller/ApiConstant.java
@@ -0,0 +1,11 @@
+package com.login.security.controller;
+
+public class ApiConstant {
+    public ApiConstant() {
+    }
+    public static final String ADD_URL = "/add";
+    public static final String GET_ALL_URL = "/getAll";
+    public static final String DELETE_URL = "/delete/{id}";
+    public static final String ADD_FORM_URL = "/addByForm";
+    public static final String GET_BY_ID = "/get/emp/{id}";
+}
