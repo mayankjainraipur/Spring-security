9b80a345b2b85ca8d273841f5891b27065e4b2f0
athorat <athorat@mocana.com>
2019-10-10 16:00:40 +0530

    security settings files

---
+++ src/main/java/com/login/security/config/PreAuthTokenHeaderFilter.java
@@ -0,0 +1,23 @@
+package com.login.security.config;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.web.authentication
+        .preauth.AbstractPreAuthenticatedProcessingFilter;
+public class PreAuthTokenHeaderFilter extends AbstractPreAuthenticatedProcessingFilter{
+
+    private String authHeaderName;
+
+    public PreAuthTokenHeaderFilter(String authHeaderName) {
+        this.authHeaderName = authHeaderName;
+    }
+
+    @Override
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader(authHeaderName);
+    }
+
+    @Override
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+}
